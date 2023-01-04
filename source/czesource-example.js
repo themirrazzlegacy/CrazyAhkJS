ebd253ae7fb981572622c8ccc7a6d0eb0ecf30f8
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-01-03 18:06:11 -0800

    Create czesource-example.js

---
+++ source/czesource-example.js
@@ -0,0 +1,26 @@
+var xy=10
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+ShowDialog("Error", "General Failure", 16, xy, xy)
+xy+=15
+Sleep(425)
+InitiateBSOD() // BSoD incoming (blue screen of death)
